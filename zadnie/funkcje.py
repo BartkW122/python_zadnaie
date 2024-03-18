696ec719133c18199932938a89500c7ada7c3ee7
Bartek <bartoszwiadernylodz@gmail.com>
2024-03-18 21:38:09 +0100

    srodki_kwadrtów

--- zadnie/funkcje.py
+++ zadnie/funkcje.py
@@ -1,32 +1,14 @@
 from tkinter import *
-def nowa_gra(root):
-    canvas = Canvas(root, width=300, height=300)
-    canvas.pack()
-    #Linie pionowe
-    canvas.create_line(100, 0, 100, 300, width=2)
-    canvas.create_line(200, 0, 200, 300, width=2)
+from math import *
 
-    # Linie poziome
-    canvas.create_line(0, 100, 300, 100, width=2)
-    canvas.create_line(0, 200, 300, 200, width=2)
-    #pola x lub o
-    canvas.create_rectangle(0, 0, 100, 100, fill="", outline="green")
-    canvas.create_rectangle(100, 0, 200, 100, fill="", outline="blue")
-    canvas.create_rectangle(200, 0, 300, 100, fill="", outline="red")
-    canvas.create_rectangle(0, 100, 100, 200, fill="", outline="pink")
-    canvas.create_rectangle(100, 100, 200, 200, fill="", outline="purple")
-    canvas.create_rectangle(200, 100, 300, 200, fill="", outline="yellow")
-    canvas.create_rectangle(0, 200, 100, 300, fill="", outline="black")
-    canvas.create_rectangle(100, 200, 200, 300, fill="", outline="magenta")
-    canvas.create_rectangle(200, 200, 300, 300, fill="", outline="white")
+
 def opcje2(root):
     root2 = Toplevel(root)
     root2.title("Opcje")
     root2.geometry("300x300")
-
     kolor = Label(root2, text="kolory:")
     kolor.pack()
-    global w
+
     w = IntVar(master=root2)
     kolor1 = Label(root2, text="zielony")
     kolor1.pack()
@@ -42,7 +24,6 @@ def opcje2(root):
     kolor3.pack()
     kolor3 = Radiobutton(root2, variable=w, value=3)
     kolor3.pack()
-    global w2
     w2 = IntVar(master=root2)
     tryb = Label(root2, text="tryb gry:")
     tryb.pack()
@@ -56,17 +37,52 @@ def opcje2(root):
     tryb2 = Radiobutton(root2, variable=w2, value=5)
     tryb2.pack()
 
-    btn = Button(root2, text="zapisz", command=lambda: zamiana_koloru(root, w))
+    btn = Button(root2, text="zapisz", command=lambda: zamiana_koloru(w))
     btn.pack()
 
     root2.mainloop()
 
 
-def zamiana_koloru(root, w):
+def zamiana_koloru(w):
     w_get = w.get()
     if w_get == 1:
-        root.configure(bg="green")
+        canvas.configure(bg="green")
     if w_get == 2:
-        root.configure(bg="blue")
+        canvas.configure(bg="blue")
     if w_get == 3:
-        root.configure(bg="red")
+        canvas.configure(bg="red")
+
+
+def nowa_gra(root):
+    global canvas
+
+    canvas = Canvas(root, width=300, height=300)
+    # canvas = Canvas(root, width=300, height=300)
+    # Linie pionowe
+    canvas.create_line(100, 0, 100, 300, width=2)
+    canvas.create_line(200, 0, 200, 300, width=2)
+
+    # Linie poziome
+    canvas.create_line(0, 100, 300, 100, width=2)
+    canvas.create_line(0, 200, 300, 200, width=2)
+    # pola x lub o
+    canvas.create_rectangle(0, 0, 100, 100, fill="", outline="green")
+    canvas.create_rectangle(100, 0, 200, 100, fill="", outline="blue")
+    canvas.create_rectangle(200, 0, 300, 100, fill="", outline="red")
+    canvas.create_rectangle(0, 100, 100, 200, fill="", outline="pink")
+    canvas.create_rectangle(100, 100, 200, 200, fill="", outline="purple")
+    canvas.create_rectangle(200, 100, 300, 200, fill="", outline="yellow")
+    canvas.create_rectangle(0, 200, 100, 300, fill="", outline="black")
+    canvas.create_rectangle(100, 200, 200, 300, fill="", outline="magenta")
+    canvas.create_rectangle(200, 200, 300, 300, fill="", outline="white")
+
+    canvas.bind("<Button-1>", dane)
+    canvas.pack()
+
+
+def dane(event):
+    x = floor(event.x / 100)
+    y = floor(event.y / 100)
+    print(x, y)
+    wynik = (100 * x) + 50
+    print("srodek", wynik)
--- zadnie/zadanko_b.py
+++ zadnie/zadanko_b.py
@@ -1,6 +1,9 @@
 from funkcje import *
 from tkinter import *
 
+global w
+global canvas
+global w2
 
 root = Tk()
 root.title("GRA")
@@ -9,11 +12,12 @@ root.geometry("300x300")
 menubar = Menu(root)
 graj = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Graj", menu=graj)
-graj.add_command(label="Nowa gra",command=lambda: nowa_gra(root))
+graj.add_command(label="Nowa gra", command=lambda: nowa_gra(root))
 graj.add_command(label="Zapisz grę")
 graj.add_command(label="Wczytaj grę")
+# graj.add_command(label="Wyjdź",command=root.destroy())
 
-# Use lambda to pass root as an argument to opcje2
+
 menubar.add_command(label="Opcje", command=lambda: opcje2(root))
 
 root.config(menu=menubar)
