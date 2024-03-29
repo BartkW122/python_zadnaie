f73249efb7808375cba95b3095dff2a8e2e2a423
Bartek <bartoszwiadernylodz@gmail.com>
2024-03-29 12:41:58 +0100

    zrobic bota i zapisywanie

--- zadnie/funkcje.py
+++ zadnie/funkcje.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from math import *
 import os
 import json
@@ -7,7 +8,9 @@ tab_dane2=[]
 tab_dane=[]
 x_z=0
 o_z=0
-def opcje2(root):
+plansza = [['' for _ in range(3)] for _ in range(3)]
+
+def opcje2(root,canvas):
     root2 = Toplevel(root)
     root2.title("Opcje")
     root2.geometry("300x300")
@@ -42,13 +45,17 @@ def opcje2(root):
     tryb2 = Radiobutton(root2, variable=w2, value=5)
     tryb2.pack()
 
-    btn = Button(root2, text="zapisz", command=lambda: zamiana_koloru(w))
+    btn = Button(root2, text="zapisz", command=lambda: zamiana_koloru(w,canvas))
     btn.pack()
 
     root2.mainloop()
+#tu trzeba zrobić \/
+def komputer():
+    global z
+    global plansza
+    messagebox.showinfo('grasz','grasz z Botem')
 
-
-def zamiana_koloru(w):
+def zamiana_koloru(w,canvas):
     w_get = w.get()
     if w_get == 1:
         canvas.configure(bg="green")
@@ -58,57 +65,107 @@ def zamiana_koloru(w):
         canvas.configure(bg="red")
 
 
-def nowa_gra(root):
-    global canvas
-    canvas = Canvas(root, width=300, height=300)
-    # canvas = Canvas(root, width=300, height=300)
-    # Linie pionowe
-    canvas.create_line(100, 0, 100, 300, width=2)
-    canvas.create_line(200, 0, 200, 300, width=2)
 
-    # Linie poziome
-    canvas.create_line(0, 100, 300, 100, width=2)
-    canvas.create_line(0, 200, 300, 200, width=2)
-
-    canvas.bind("<Button-1>", dane)
-    canvas.pack()
-
-
-def dane(event):
+def nowa_gra(root, canvas):
+    global plansza
+    global z
+    global tab_dane
+    global tab_dane2
+    tab_dane=[]
+    tab_dane2=[]
+    plansza = [['' for _ in range(3)] for _ in range(3)]
+    canvas.delete("all")
+    for linia in range(1, 3):
+        canvas.create_line(linia * 100, 0, linia * 100, 300, fill="black", width=5)
+        canvas.create_line(0, linia * 100, 300, linia * 100, fill="black", width=5)
+    z = "x"
+    canvas.bind("<Button-1>", lambda event: dane(event, canvas))
+def dane(event,canvas):
     global z
     global wynik
     global wynik2
-    global tab_dane
-    global tab_dane2
+    global x
+    global y
 
     x = floor(event.x / 100)
     y = floor(event.y / 100)
     wynik = (100 * x) + 50
     wynik2 = (100 * y) + 50
 
-    tab_dane=[wynik, wynik2]
+    z = ruch(wynik, wynik2, z,canvas)
+
+def sprawdz_czy_puste():
+    global plansza
+    global x
+    global y
+    global z
+    #print(plansza)
+    if plansza[x][y] == '':
+        plansza[x][y] = z
+        return True
+    else:
+        return False
+
+
+
+def wygrana():
+    global plansza
+    for i in range(3):
+        if plansza[i][0] == plansza[i][1] == plansza[i][2] != '':
+            return True
+        if plansza[0][i] == plansza[1][i] == plansza[2][i] != '':
+            return True
+    if plansza[0][0] == plansza[1][1] == plansza[2][2] != '':
+        return True
+    if plansza[0][2] == plansza[1][1] == plansza[2][0] != '':
+        return True
+    return False
+
+def remis():
+    global plansza
+    for row in plansza:
+        for znak2 in row:
+            if znak2 == '':
+                return False
+    return True
+
+
+def zapisywanie_danych():
+    global wynik
+    global wynik2
+    global tab_dane
+    global tab_dane2
+    tab_dane = [wynik, wynik2]
 
     tab_dane2.append(tab_dane)
-
-    print(tab_dane2)
-
-    z = znak(wynik, wynik2, z)
-
-
-def znak(wynik, wynik2, znak):
-    global canvas
+    #print(tab_dane2)
+def ruch(wynik, wynik2, znak,canvas):
     global x_z
     global o_z
     global tab_dane2
-    #if x_z==x_z or o_z==o_z:
-        #print("miejsce zajęte")
-    #else:
-    if znak == "x":
-        x_z = canvas.create_line(wynik + 20, wynik2 - 20, wynik - 20, wynik2 + 20, fill="black", width=10), canvas.create_line(wynik - 20, wynik2 - 20, wynik + 20, wynik2 + 20, fill="black", width=10)
-        return "o"
+    global x
+    global y
+
+    if sprawdz_czy_puste():
+        zapisywanie_danych()
+        plansza[x][y]=znak
+        if wygrana():
+            messagebox.showinfo('Wygrana', f'Gracz {znak} wygrywa!')
+        if remis():
+            messagebox.showinfo('remis','remis')
+        if znak == "x":
+            x_z = canvas.create_line(wynik + 20, wynik2 - 20, wynik - 20, wynik2 + 20, fill="black", width=10), canvas.create_line(wynik - 20, wynik2 - 20, wynik + 20, wynik2 + 20, fill="black", width=10)
+            return "o"
+        else:
+            o_z = canvas.create_oval(wynik - 20, wynik2 - 20, wynik + 20, wynik2 + 20, outline="black", width=10)
+            return "x"
     else:
-        o_z = canvas.create_oval(wynik - 20, wynik2 - 20, wynik + 20, wynik2 + 20, outline="black", width=10)
-        return "x"
+        messagebox.showinfo('miejsce', 'miejsce jest zajete')
+        return znak
+
+
+
+
 def zapisz_gre():
     global tab_dane2
     with open('zapisana_gra.json', 'a') as file:
--- zadnie/zadanko_b.py
+++ zadnie/zadanko_b.py
@@ -10,17 +10,19 @@ global event
 root = Tk()
 root.title("GRA")
 root.geometry("300x300")
+canvas = Canvas(root, width=300, height=300)
+canvas.pack()
 
 menubar = Menu(root)
 graj = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Graj", menu=graj)
-graj.add_command(label="Nowa gra", command=lambda: nowa_gra(root))
+graj.add_command(label="Nowa gra", command=lambda: nowa_gra(root,canvas))
 graj.add_command(label="Zapisz grę",command=zapisz_gre)
 graj.add_command(label="Wczytaj grę",command=wczytaj_gre)
 graj.add_command(label="Wyjdź",command=root.destroy)
 
 
-menubar.add_command(label="Opcje", command=lambda: opcje2(root))
+menubar.add_command(label="Opcje", command=lambda: opcje2(root,canvas))
 
 root.config(menu=menubar)
 root.mainloop()
