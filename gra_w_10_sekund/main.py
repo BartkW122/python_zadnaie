7403e94c4a77a07f83d53cdb8b35a60737e42baa
BartkW122 <154226682+BartkW122@users.noreply.github.com>
2024-04-26 10:43:02 +0200

    dodanie znaku

--- gra_w_10_sekund/main.py
+++ gra_w_10_sekund/main.py
@@ -3,7 +3,17 @@ from tkinter import *
 
 start_czasu = 0
 stop_czasu = 0
+tab_czas=[]
 
+def klik(event):
+    print("kliknięto")
+def przycisk(event):
+    gracz_1=g1.get()
+    print(f"gracz 1 ma przycisk {gracz_1}")
+    okno.bind(f"{gracz_1}", klik)
+    g1.config(state= "disabled")
+def zmien():
+    g1.config(state="enable")
 def start():
     global start_czasu
     start_czasu = time.time()
@@ -15,6 +25,7 @@ def stop():
         stop_czasu = time.time()
         zapisany_czas = stop_czasu - start_czasu
         print("Minęło:", round(zapisany_czas, 2), "sekund")
+        tab_czas.append(round(zapisany_czas, 2))
         start_czasu = 0
 
 def czas():
@@ -25,16 +36,27 @@ def czas():
         if zapisany_czas < 10:
             okno.after(100, czas)
         else:
-            print("Minęło dokładnie 10 sekund!")
+            print("Minęło  10 sekund!")
+def pokaz_zapisaany_czas():
+    print("zapisany czas "+str(tab_czas))
 
 okno = Tk()
 okno.title("Gra w 10 sekund")
 okno.geometry("300x300")
-
+gracz1=Label(okno,text="podaj znak dla gracza 1:")
+gracz1.pack()
+g1=Entry(okno)
+g1.pack()
+zapisz=Button(okno,text="zapisz",command=lambda:przycisk(Event))
+zapisz.pack()
+#okno.bind("a", przycisk)
 start_button = Button(okno, text="Start", command=start)
 start_button.pack()
 
 stop_button = Button(okno, text="Stop", command=stop)
 stop_button.pack()
+
+pokaz_czas = Button(okno, text="pokaz", command=pokaz_zapisaany_czas)
+pokaz_czas.pack()
 
 okno.mainloop()
