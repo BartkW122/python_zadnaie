1bcc59e446f917aa41481d1fed0ad8597e487f2a
Bartek <bartoszwiadernylodz@gmail.com>
2024-05-14 17:39:11 +0200

    nic

--- gra_w_10_sekund/main.py
+++ gra_w_10_sekund/main.py
@@ -3,38 +3,105 @@ from tkinter import *
 
 start_czasu = 0
 stop_czasu = 0
+tab_graczy = []
+tab_czas = []
+czas_g1 = 0
+czas_g2 = 0
+en = []
+
+
+def gracz():
+    ilosc_g = ilosc_graczy.get()
+    ilosc_g_2 = int(ilosc_g)
+
+    for i in range(1, ilosc_g_2 + 1):
+        print(i)
+        Label(okno, text=f"podaj znak dla gracza {i}:").pack()
+        e = Entry(okno)
+        e.pack()
+        en.append(e)
+
+
+def przycisk(event):
+    for i in range(len(en)):
+        gracz_btn = en[i].get()
+        print(f"gracz {i} ma przycisk {gracz_btn}")
+        okno.bind(f"gracz{i}", stop)
+
+        en[i].config(state="disabled")
+
+
+def zmien():
+    for i in range(len(en)):
+        en[i].config(state="normal")
+
 
 def start():
     global start_czasu
+    for i in range(len(en)):
+        okno.bind(f"gracz{i}", stop)
     start_czasu = time.time()
     czas()
 
-def stop():
+
+def stop(event):
     global start_czasu, stop_czasu
     if start_czasu != 0:
         stop_czasu = time.time()
         zapisany_czas = stop_czasu - start_czasu
         print("Minęło:", round(zapisany_czas, 2), "sekund")
-        start_czasu = 0
+        for i in range(len(en)):
+            if event.char == en[i].get():
+                tab_czas.append(round(zapisany_czas, 2))
+                print(f"klikniento g{i}")
+                okno.unbind(f"gracz{i}")
+
 
 def czas():
-    global start_czasu
+    global start_czasu, czas_g1, czas_g2
     if start_czasu != 0:
         zapisany_czas = time.time() - start_czasu
         print("Pozostało:", round(zapisany_czas, 2), "sekund")
-        if zapisany_czas < 10:
+        if zapisany_czas < 15:
             okno.after(100, czas)
         else:
-            print("Minęło dokładnie 10 sekund!")
+            print("Minęło  15 sekund!")
 
+
+def pokaz_zapisaany_czas():
+    print("zapisany czas_g1 " + str(czas_g1))
+    print("zapisany czas_g2 " + str(czas_g2))
+    print(en)
+    wygrana()
+
+
+# def wygrana():
+# if czas_g1>czas_g2:
+# print("gracz 1 wygrał")
+# if czas_g2>czas_g1:
+# print("gracz 2 wygrał")
 okno = Tk()
 okno.title("Gra w 10 sekund")
-okno.geometry("300x300")
+okno.geometry("800x800")
+
+ilosc_graczy = Label(okno, text="podaj ilosc graczy:")
+ilosc_graczy.pack()
+ilosc_graczy = Entry(okno)
+ilosc_graczy.pack()
+
+stwoz_graczy = Button(okno, text="stworz graczy", command=gracz)
+stwoz_graczy.pack()
 
 start_button = Button(okno, text="Start", command=start)
 start_button.pack()
 
-stop_button = Button(okno, text="Stop", command=stop)
-stop_button.pack()
+pokaz_czas = Button(okno, text="pokaz_czas", command=pokaz_zapisaany_czas)
+pokaz_czas.pack()
+
+zapisz = Button(okno, text="zapisz_znak", command=lambda: przycisk(Event))
+zapisz.pack()
+
+zmien = Button(okno, text="zmien_znak", command=zmien)
+zmien.pack()
 
 okno.mainloop()
